ff5dba03762d8c95c9fb82c394b549c571599fb7
mahenajT <mahenaj200815@gmail.com>
2024-08-06 07:25:38 +0600

    01_Template Literals

---
+++ Day 7/Youtube/01_Template Literals.js
@@ -0,0 +1,24 @@
+
+const message1 = 
+" This is my \n" + 
+"\"first\" message"
+console.log(message1);
+
+
+// Template Literals
+const message2 = 
+`This is 
+my "first"
+message`
+console.log(message2);
+
+const name = "John"
+const message3 = 
+`Hi ${name}!
+
+Thank you for joining my mail listing`
+console.log(message3);
+
+// Object Literals {}
+// Boolean Literals true, false
+// String Literals "", '' 
